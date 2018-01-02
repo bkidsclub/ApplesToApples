c346cbf7aa50544de2d7d6ed0a6abb7d06e0468e
WhatQuestionMark <maxi.balabanski@gmail.com>
2018-01-01 20:46:18 -0500

    I fixed this edgy meemr up abit

--- apples/GUI.py
+++ apples/GUI.py
@@ -1,15 +1,36 @@
-from Tkinter import *
-import Tkinter as tk
+from tkinter import *
+import tkinter as tk
+#Creating the Canvas
 root = tk.Tk ()
-canvas = Canvas(root, width=1280, height=1080, bg="black")
-image1 = tk.PhotoImage(file="C:\Users\Happy\Desktop\Programming\Apples To Apples\Table.gif")
-w = image1.width()
-h = image1.height()
-panel1 = tk.Label(root, image=image1)
-panel1.pack(side='top', fill='both', expand='yes')
-panel1.image = image1
+canvas = Canvas(root, width=1280, height=1080, bg="#cc9900")
 canvas.pack()
-canvas.create_rectangle( 300, 250, 600, 500, fill="#33cc33", outline="white", width=4)
-canvas.create_text(450, 350, fill="white", font="Times 20 italic bold",
-                   text="Gapple Function")
+#Green Card
+canvas.create_rectangle( 300, 300, 650, 600, fill="#33cc33", outline="white", width=4)
+#Choices
+canvas.create_text(50, 10, fill="white", font="Times 20 italic bold",
+                   text="Rapple1")
+canvas.create_text(50, 30, fill="white", font="Times 20 italic bold",
+                   text="Rapple2")
+canvas.create_text(50, 50, fill="white", font="Times 20 italic bold",
+                   text="Rapple3")
+canvas.create_text(50, 70, fill="white", font="Times 20 italic bold",
+                   text="Rapple4")
+canvas.create_text(50, 90, fill="white", font="Times 20 italic bold",
+                   text="Rapple5")
+canvas.create_text(50, 110, fill="white", font="Times 20 italic bold",
+                   text="Rapple6")
+canvas.create_text(50, 130, fill="white", font="Times 20 italic bold",
+                   text="Rapple7")
+#Buttons to choose the word
+master = Tk()
+b = Button(master, text="OK", command=print("good"))
+b.pack()
+#Red Cards
+canvas.create_rectangle( 750, 700, 950, 900, fill="#ff3300", outline="white", width=4)
+canvas.create_rectangle( 800, 700, 1000, 900, fill="#ff3300", outline="white", width=4)
+canvas.create_rectangle( 850, 700, 1050, 900, fill="#ff3300", outline="white", width=4)
+canvas.create_rectangle( 900, 700, 1100, 900, fill="#ff3300", outline="white", width=4)
+canvas.create_rectangle( 950, 700, 1150, 900, fill="#ff3300", outline="white", width=4)
+canvas.create_rectangle( 1000, 700, 1200, 900, fill="#ff3300", outline="white", width=4)
+canvas.create_rectangle( 1050, 700, 1250, 900, fill="#ff3300", outline="white", width=4)
 root.mainloop()
